862e1df004b3bd93985db24713cd17f5f0547f65
Marco Giugliani <marco.giugliani@studio.unibo.it>
2023-11-22 19:53:55 +0100

    Implemented SimpleGUI class in exercise 93

--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,15 @@
 package it.unibo.mvc;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.io.IOException;
+
 
 /**
  * A very simple program using a graphical interface.
@@ -8,6 +17,39 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
-    private final JFrame frame = new JFrame();
+    private final JFrame frame = new JFrame("My first Java graphical interface");
+    private static final int PROPORTION = 3;
 
+    public SimpleGUI(final Controller controller) {
+        final JPanel canvas = new JPanel();
+        canvas.setLayout(new BorderLayout());
+        final JTextArea textArea = new JTextArea();
+        canvas.add(textArea, BorderLayout.CENTER);
+        final JButton btnSave = new JButton("Save");
+        canvas.add(btnSave, BorderLayout.SOUTH);
+
+        btnSave.addActionListener(al -> {
+            try {
+                controller.saveContent(textArea.getText());
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(frame, e.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
+            }
+        });
+
+        frame.setContentPane(canvas);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationByPlatform(true);
+        frame.pack();
+
+        final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int)screenSize.getWidth();
+        final int sh = (int)screenSize.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new SimpleGUI(new Controller());
+    }
 }
