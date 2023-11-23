87d77ee31bbd0955a0ae15eeaf3dedab599fe41b
Marco Giugliani <marco.giugliani@studio.unibo.it>
2023-11-23 17:51:45 +0100

    Implemented the controller interface and class in exercise 94

--- 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,33 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
- *
+ * Simple controller of I/O access
  */
 public interface Controller {
+    /**
+     * Sets the next string to print
+     * @param next The next string to print 
+     * @throws NullPointerException If the string to print is null
+     */
+    void setNextToPrint(String next) throws NullPointerException;
 
+    /**
+     * Gets the next string to print
+     * @return The next string to print
+     */
+    String getNextToPrint();
+
+    /**
+     * Gets the history of the printed strings
+     * @return A list of strings in which there are all the printed strings
+     */
+    List<String> getAllPrintedStrings();
+
+    /**
+     * Prints the current string
+     * @throws IllegalStateException If the current string is unset
+     */
+    void printCurrentString() throws IllegalStateException;
 }
--- 94-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,45 @@
 package it.unibo.mvc;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
 /**
- * 
- *
+ * A simple controller that prints the current string and saves all the printed ones
  */
 public final class SimpleController implements Controller {
 
+    private String nextToPrint;
+    private final List<String> printedStrings = new ArrayList<>();
+
+    @Override
+    public void setNextToPrint(final String next) throws NullPointerException {
+        this.nextToPrint = Objects.requireNonNull(next, "It cannot be set a null string");
+    }
+
+    @Override
+    public String getNextToPrint() {
+        if (this.nextToPrint == null) {
+            throw new IllegalStateException("The next string to print is unset");
+        }
+        return this.nextToPrint;
+    }
+
+    @Override
+    public List<String> getAllPrintedStrings() {
+        if (this.printedStrings.isEmpty()) {
+            throw new IllegalStateException("There are no printed strings");
+        }
+        return Collections.unmodifiableList(this.printedStrings);
+    }
+
+    @Override
+    public void printCurrentString() throws IllegalStateException {
+        if (this.nextToPrint == null) {
+            throw new IllegalStateException("The string to print is unset");
+        }
+        this.printedStrings.add(this.nextToPrint);
+        System.out.println(this.nextToPrint);   
+    }
 }
